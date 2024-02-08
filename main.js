7d0df6684bb863ddf154dddc42fb84c7a70f2744
Lucas Nakeli <daddymakeli@gmail.com>
2024-02-08 21:33:07 +0200

    Refactor code to support traffic simulation

--- main.js
+++ main.js
@@ -4,16 +4,16 @@ canvas.width = 200;
 
 const ctx = canvas.getContext("2d");
 const road = new Road(canvas.width / 2, canvas.width * 0.9);
-const car = new Car(road.getLaneCenter(1), 100, 30, 50);
-const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50)];
+const car = new Car(road.getLaneCenter(1), 100, 30, 50, "KEYS");
+const traffic = [new Car(road.getLaneCenter(1), -100, 30, 50, "DUMMY", 2)];
 
 animate();
 
 function animate() {
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].update(road.borders);
+    traffic[i].update(road.borders, []);
   }
-  car.update(road.borders);
+  car.update(road.borders, traffic);
 
   canvas.height = window.innerHeight;
   ctx.save();
@@ -21,9 +21,9 @@ function animate() {
 
   road.draw(ctx);
   for (let i = 0; i < traffic.length; i++) {
-    traffic[i].draw(ctx);
+    traffic[i].draw(ctx, "lightblue");
   }
-  car.draw(ctx);
+  car.draw(ctx, "blue");
   ctx.restore();
   requestAnimationFrame(animate);
 }
